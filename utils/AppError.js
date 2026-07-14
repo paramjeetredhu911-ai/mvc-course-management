ffe3d1bc3719eaf9ba6c3f0a5e6d412f577d11af
paramjeetredhu911-ai <paramjeetredhu911@gmail.com>
2026-07-14 16:16:51 +0530

    Update AppError.js

--- utils/AppError.js
+++ utils/AppError.js
@@ -1,1 +1,12 @@
-console.log("AppError custom utility initialized.");
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // Marks errors as predictable operational events
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
