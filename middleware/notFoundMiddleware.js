6b5ea3163bc9a57edd14c0f1280c7f0360f132ab
paramjeetredhu911-ai <paramjeetredhu911@gmail.com>
2026-07-14 16:19:00 +0530

    Update notFoundMiddleware.js

--- middleware/notFoundMiddleware.js
+++ middleware/notFoundMiddleware.js
@@ -1,1 +1,6 @@
-console.log("notFoundMiddleware fallback handler initialized.");
+const AppError = require('../utils/AppError');
+
+module.exports = (req, res, next) => {
+  // Triggers an AppError instances configuration
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+};
